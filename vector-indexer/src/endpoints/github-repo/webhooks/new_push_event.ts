3ddb13605854536b6babb9058b07d336b4d573f4
Manuel Reyes (23people.io) <manuel@23people.io>
2025-01-10 15:20:16 -0300

    new version

--- vector-indexer/src/endpoints/github-repo/webhooks/new_push_event.ts
+++ vector-indexer/src/endpoints/github-repo/webhooks/new_push_event.ts
@@ -1,35 +1,39 @@
 import { createFactory } from 'hono/factory'
 import { logger } from 'hono/logger'
-import { upsert_vector} from '../../../services/vectorize-indexer'
+import { upsert_vectors } from '../../../services/vectorize-indexer'
 
 const factory = createFactory<{ Bindings: Env }>()
 
 const upsert_documents = factory.createHandlers(logger(), async (c) => {
     const { commits } = await c.req.json();
+    const results = [];
+
     if (!commits) {
         return c.text("Missing input data (commits)", 400);
     }
 
-    const { added, removed, modified } = commits;
-
-    if (!added && !removed && !modified) {
-        return c.text("Nothing to index", 200);
-    }
+    const { added, removed, modified } = commits[0];
 
     if (added) {
         // index new files
-        upsert_vectors(added, c.env);
+        const added_results = await upsert_vectors(added, c);
+        results.push(added_results);
     }
 
     if (modified) {
         // index modified files
+        const modified_results = await upsert_vectors(modified, c);
+        results.push(modified_results);
     }
 
     if (removed) {
         // remove deleted files from index
+        const removed_results = await upsert_vectors(removed, c);
+        results.push(removed_results);
+
     }
-    
-    return c.json({ input_data });
+
+    return c.json(results);
 });
 
 export default upsert_documents;
--- vector-indexer/src/services/vectorize-indexer/index.ts
+++ vector-indexer/src/services/vectorize-indexer/index.ts
@@ -1,25 +1,62 @@
+import { Context } from "hono";
+import { App } from "octokit";
 
-export default {
-  async upsert_vectors(paths: string[], env: Env) {
+async function upsert_vectors(paths: string[], c: Context): Promise<{ id: string; path: string; inserted: any }[]> {
 
-    
-    const { data } = await env.AI.run("@cf/baai/bge-base-en-v1.5", {
-        text: [text],
-    });
-    const values = data[0];
+    const results = [];
 
-    if (!values) {
-        throw new Error("Failed to generate vector embedding");
+    for (const path of paths) {
+        if (!path.startsWith(c.env.HANDBOOK_REPO_FILTER_PATH)) {
+            continue;
+        }
+        const content = await get_content(path, c);
+
+        const { data } = await c.env.AI.run("@cf/baai/bge-base-en-v1.5", {
+            text: [content],
+        });
+        const values = data[0];
+
+        if (!values) {
+            throw new Error("Failed to generate vector embedding");
+        }
+
+        const id = path;
+        const inserted = await c.env.HANDBOOK_INDEX.upsert([
+            {
+                id: id.toString(),
+                values,
+            },
+        ]);
+
+        results.push({ id, path, inserted });
     }
 
-    const { id } = path;
-    const inserted = await env.HANDBOOK_INDEX.upsert([
-        {
-            id: id.toString(),
-            values,
-        },
-    ]);
+    return results;
+}
 
-    return c.json({ id, text, inserted });
-  },
-};
+async function get_content(path: string, c: Context): Promise<string> {
+
+    const github_app = new App({
+        appId: c.env.VECTOR_INDEXER_GITHUB_APP_ID,
+        privateKey: c.env.VECTOR_INDEXER_GITHUB_APP_PRIVATE_KEY,
+
+    });
+
+    const octokit = await github_app.getInstallationOctokit(c.env.VECTOR_INDEXER_GITHUB_APP_INSTALLATION_ID);
+
+    const response = await octokit.rest.repos.getContent({
+        owner: c.env.HANDBOOK_REPO_OWNER,
+        repo: c.env.HANDBOOK_REPO_NAME,
+        path,
+
+    });
+
+    if (response.status !== 200) {
+        throw new Error(`Failed to fetch content from ${path}: ${response.status}`);
+    }
+
+    return response.data.toString();
+}
+
+export { upsert_vectors };
+
