e4ff76878eabdd349d411681628f7ff50462d7c0
Manuel Reyes (23people.io) <manuel@23people.io>
2025-02-13 15:06:00 -0300

    preparing for new version

---
+++ cf-workers/docs-loader/scripts/colors.sh
@@ -0,0 +1,8 @@
+# ANSI color codes
+MAGENT=$(tput -Txterm setaf 5)
+GREEN=$(tput -Txterm setaf 2)
+YELLOW=$(tput -Txterm setaf 3)
+WHITE=$(tput -Txterm setaf 7)
+RED=$(tput -Txterm setaf 1)
+BLUE=$(tput -Txterm setaf 6)
+RESET=$(tput -Txterm sgr0)
