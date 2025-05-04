6421c887c88dfb2b67930dcc895a2f4bf220cdbf
Manuel Reyes <manuel@23people.io>
2025-05-04 15:01:06 -0400

    bump version to 1.2.57

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -11,12 +11,12 @@ cd "$(dirname "$0")/.."
 
 echo "${MAGENT}Bumping package version...${RESET}"
 
+git add .
 echo "${YELLOW}Pre: Committing previous changes...${RESET}"
 if git diff --quiet; then
   echo "${WHITE}No changes to commit before version bump.${RESET}"
 else
   echo "${WHITE}Preparing for new version...${RESET}"
-  git add .
   git commit -m "preparing for new version"
 fi
 
